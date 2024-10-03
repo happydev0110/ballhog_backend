b08c0e1eae28dfcc4f3a26c78b7bdb01587928c5
supergoldendragon <happydev0110@gmail.com>
2024-10-03 18:53:47 -0500

    update

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-// Middleware
-app.use(cors());
-app.use(express.json());
-// use routes
-app.use('/api', userRoutes);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Required to handle the path for ES module usage
 const __filename = fileURLToPath(import.meta.url);
@@ -31,6 +33,13 @@ app.use(express.static(join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'build', 'index.html'));
 });
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+// use routes
+app.use('/api', userRoutes);
+
 
 // app.get('/test', (req, res) => {
 //   res.json({ message: 'Server is running' });
