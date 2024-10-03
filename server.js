3736ed0d7a6122247f3db0da9b1ac1108204e89e
supergoldendragon <happydev0110@gmail.com>
2024-10-03 15:53:08 -0500

    update

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import userRoutes from './server/routes/userRoutes.js';
 import dotenv from 'dotenv';
+
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+import userRoutes from './server/routes/userRoutes.js';
 import connectDB from './server/utils/connect-mongo.js';
 
 dotenv.config();
@@ -14,6 +19,18 @@ app.use(cors());
 app.use(express.json());
 // use routes
 app.use('/api', userRoutes);
+
+// Required to handle the path for ES module usage
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the React build folder
+app.use(express.static(join(__dirname, 'build')));
+
+// Catch-all handler to serve index.html for any route
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'build', 'index.html'));
+});
 
 // app.get('/test', (req, res) => {
 //   res.json({ message: 'Server is running' });
