bf19b1ca03238e4f815d8398e06d49b85d2d1b22
supergoldendragon <happydev0110@gmail.com>
2024-10-04 10:56:48 -0500

    make it better

--- server.js
+++ server.js
@@ -16,11 +16,7 @@ const app = express();
 
 // CORS Options
 const corsOptions = {
-  origin: [
-    // 'https://playballhog.com',
-    // 'https://www.playballhog.com',    // Your custom domain
-    'https://ballhog-87d4f254b843.herokuapp.com', // Heroku app
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,  // Allow credentials if needed
 };
