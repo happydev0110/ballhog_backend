690e844a453583faf9f70626db0017bf3cef785e
supergoldendragon <happydev0110@gmail.com>
2024-10-28 06:44:27 -0500

    update

--- server/models/users.js
+++ server/models/users.js
@@ -2,48 +2,18 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      default: ''
-    },
-    email: {
-      type: String,
-      unique: true,
-    },
-    password: {
-      type: String,
-    },
-    phoneNumber: {
-      type: String,
-    },
-    balance: {
-      type: Number,
-      required: true,
-      default: 50
-    },
-    payment:{
-      venmo: {
-        type: String,
-        default: ''
-      },
-      paypal: {
-        type: String,
-        default: ''
-      },
-      cashApp: {
-        type: String,
-        default: ''
-      },
-      zelle: {
-        type: String,
-        default: ''
-      }
+    name: { type: String, required: true, default: '' },
+    email: { type: String }, // Removed unique constraint
+    password: { type: String },
+    phoneNumber: { type: String, default: '123-456-7890' }, // Removed unique constraint
+    balance: { type: Number, required: true, default: 50 },
+    payment: {
+      venmo: { type: String, default: '' },
+      paypal: { type: String, default: '' },
+      cashApp: { type: String, default: '' },
+      zelle: { type: String, default: '' }
     }
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
-
 export default mongoose.model("User", userSchema);
