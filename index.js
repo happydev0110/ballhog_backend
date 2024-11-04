1bcad777afc593411bb2d254d0e9e6140038298a
supergoldendragon <happydev0110@gmail.com>
2024-11-04 13:36:05 -0600

    updat

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { join } from 'path';
@@ -17,13 +16,13 @@ const app = express();
 
 // app.use(sslRedirect());
 app.use(sslRedirect.default());
-// app.use((req, res, next) => {
-//   if (req.header('x-forwarded-proto') !== 'https') {
-//     res.redirect(`https://${req.header('host')}${req.url}`);
-//   } else {
-//     next();
-//   }
-// });
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.header('host')}${req.url}`);
+  } else {
+    next();
+  }
+});
 
 app.use(cors());
 app.use(express.json());
